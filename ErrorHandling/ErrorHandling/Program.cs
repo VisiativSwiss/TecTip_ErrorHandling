bd726619ac64c9426d17de843adff4fb04dc9e3e
Martin Bartolomé <martin.bartolome@visiativ.com>
2023-11-01 11:05:14 +0100

    Init

---
+++ ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,56 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+
+namespace ErrorHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SldWorks solidworksApp = new SldWorks();
+            try
+            {
+                // Attempt to add a new feature to the part
+                var part = solidworksApp.ActiveDoc as PartDoc;
+                if (part == null)
+                {
+                    throw new InvalidOperationException("No active document found.");
+                }
+                part.AddFeatureXYZ(); // Hypothetical method that adds a feature
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Specific handling for InvalidOperationException
+                LogError(ex);
+                UserFeedback("The document is not loaded correctly.");
+            }
+            catch (Exception ex)
+            {
+                // General error handling for all other types of exceptions
+                LogError(ex);
+                UserFeedback("An unexpected error has occurred.");
+            }
+            finally
+            {
+                // Cleanup operations
+                Cleanup();
+            }
+        }
+
+        private static void Cleanup()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        private static void LogError(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        private static void UserFeedback(string message)
+        {
+            Console.Write("Feedback: " + message);
+        }
+    }
+}
---
+++ ErrorHandling/ErrorHandling/partdocextension.cs
@@ -0,0 +1,13 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+
+namespace ErrorHandling
+{
+    public static class partdocextension
+    {
+        public static void AddFeatureXYZ(this PartDoc partDoc)
+        {
+            throw new Exception("Here is my Error");
+        }
+    }
+}
